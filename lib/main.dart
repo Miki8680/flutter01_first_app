7e1747c81604952a28428df820a5df87be1f7e1d
Miki8680 <mikmm8680@gmail.com>
2023-07-31 22:53:56 +0300

    add plug instead favorite page

--- lib/main.dart
+++ lib/main.dart
@@ -54,9 +54,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   var selectedIndex = 0;     // ← Add this property.
-
+  
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+    
     return Scaffold(
       body: Row(
         children: [
@@ -87,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ),
           ),
         ],
